8d7ca6e4874143f257e382564570e58ec8c1d929
Alfonso Olivares <dream@Mac-mini.local>
2021-01-07 10:13:50 -0600

    Redirect

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // console.log(req.originalUrl)
+
+  if(err.status === 401){
+    return res.redirect('/');
+  }
+
   res.status(err.status || 500);
   res.render('error');
 });
